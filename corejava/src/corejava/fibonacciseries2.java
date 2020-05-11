fe510432adeb9fe3c9cab353fc59f84563122bb8
BhuvaneshWaran <BhuvaneshWaran@Bhuvi3411.lan>
2020-05-11 08:13:51 -0700

    New Program Added

--- corejava/src/corejava/fibonacciseries2.java
+++ corejava/src/corejava/fibonacciseries2.java
@@ -3,7 +3,9 @@ package corejava;
 public class fibonacciseries2 {
 
 	public static void main(String[] args) {
-		int f=0,s=1;
+		
+		// type 1:
+		/*int f=0,s=1;
 		int t=f+s;
 		for (int i=0; i<=20;  i++)
 		{
@@ -12,11 +14,41 @@ public class fibonacciseries2 {
 			f=s;
 			s=t;
 			
-			if(f==1)
+			if(f==78)
 			{
 				break;
 			} 
 
 	}
+*/		
+		// type 2:
+		
+		int n1=0,n2=1,sum=0;
+		
+		System.out.print(n1+ " "+ n2);
+		
+		for(int i=2;i<10;i++)
+		{
+			sum=n1+n2;           // 0+1
+			System.out.print(" " +sum);
+			n1=n2;              //  n1=1
+			n2=sum;             //   n2=1
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
